865f3232374d041c4f54737aa0ce955a3929f213
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2024-11-23 02:04:04 +0100

    simple client

---
+++ godaft.go
@@ -0,0 +1,42 @@
+package godaft
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/valyala/fastjson"
+)
+
+const endpoint string = "https://gateway.daft.ie/old/v1/listings"
+
+func DoRequest(payload map[string]any) (*fastjson.Value, error) {
+	jsonBody, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyReader := bytes.NewReader(jsonBody)
+	req, err := http.NewRequest(http.MethodPost, endpoint, bodyReader)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("brand", "daft")
+	req.Header.Set("platform", "web")
+
+	httpResult, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	resultBody, err := io.ReadAll(httpResult.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var p fastjson.Parser
+	return p.ParseBytes(resultBody)
+}
